6046c327ff5e4e863d0496dbc25afb8453684add
Sahand Omarzai <a22ahmom@student.his.se>
2023-04-06 09:49:16 +0200

    added the view Button inside the LinearLayout by simply implementing some code and also imported the Button and Log class.The view Button have a clickListener so when the user clicks on the button a text will be displayed on LogCat informing the user that they have clicked on the button.

--- app/src/main/java/com/example/widgets/MainActivity.java
+++ app/src/main/java/com/example/widgets/MainActivity.java
@@ -1,4 +1,7 @@
 package com.example.widgets;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,5 +14,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button b = findViewById(R.id.myNewBtn);
+
+        b.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("==>","We clicked on Button!");
+            }
+        });
     }
 }
